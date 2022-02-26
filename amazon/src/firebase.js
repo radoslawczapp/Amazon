316831d39047ba5ec1b80a241a1286d242330f4d
czappradoslaw <czappradoslaw@gmail.com>
2022-02-26 20:01:30 +0100

    Node core amazon app created

---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNk3dsSe0r8JfXFLBjZv4DBjWeGOwcobI",
+    authDomain: "fir-6c627.firebaseapp.com",
+    projectId: "fir-6c627",
+    storageBucket: "fir-6c627.appspot.com",
+    messagingSenderId: "570436562256",
+    appId: "1:570436562256:web:6e4d134d37e1998b805055",
+    measurementId: "G-VPK0EQMRQF"
+  };
